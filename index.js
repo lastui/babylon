3997846d85a865b6f161b1738d2819aadb2e85ca
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-03-17 07:54:30 +0100

    upgrade versions and drop stages

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const plugins = [
-
-  // Stage 0
   '@babel/plugin-proposal-function-bind',
-
-  // Stage 1
   '@babel/plugin-proposal-export-default-from',
   '@babel/plugin-proposal-logical-assignment-operators',
   [
@@ -13,20 +9,12 @@ const plugins = [
     },
   ],
   [
-    '@babel/plugin-proposal-pipeline-operator',
-    {
-      proposal: 'minimal',
-    },
-  ],
-  [
     '@babel/plugin-proposal-nullish-coalescing-operator',
     {
       loose: false,
     },
   ],
   '@babel/plugin-proposal-do-expressions',
-
-  // Stage 2
   [
     '@babel/plugin-proposal-decorators',
     {
@@ -37,8 +25,6 @@ const plugins = [
   '@babel/plugin-proposal-export-namespace-from',
   '@babel/plugin-proposal-numeric-separator',
   '@babel/plugin-proposal-throw-expressions',
-
-  // Stage 3
   '@babel/plugin-syntax-dynamic-import',
   '@babel/plugin-syntax-import-meta',
   [
